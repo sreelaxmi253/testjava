791121449de8c570e11e65e809750216114c9684
sreelaxmi253 <sreelaxmi253@gmail.com>
2019-06-12 21:53:46 +0530

    First Commit

---
+++ src/java12/TestJava.java
@@ -0,0 +1,15 @@
+package java12;
+
+public class TestJava {
+
+	public static void main(String[] args)
+	{
+		
+		int num1=5,num2=8,sum;
+		sum=num1+num2;
+		System.out.println("sum of "+num1+" and "+num2+" is "+sum);
+		
+
+	}
+
+}
